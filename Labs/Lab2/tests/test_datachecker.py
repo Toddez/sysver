aab76dd8c635744874aacdce869ae5eb02fd5b96
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-06 19:25:24 +0200

    Added metod test_check_valid_age

--- Labs/Lab2/tests/test_datachecker.py
+++ Labs/Lab2/tests/test_datachecker.py
@@ -3,3 +3,10 @@ from datachecker import DataChecker
 class TestDataChecker:
     pass
 
+    def test_check_valid_age(self):
+        dc = DataChecker()
+
+        assert dc.check_valid_age(1) == True
+        assert dc.check_valid_age(-1) == False
+        assert dc.check_valid_age(1.5) == False
+        assert dc.check_valid_age("one") == False
