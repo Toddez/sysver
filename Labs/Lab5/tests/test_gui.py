3fda00b25ae75b1645752726bbaf6a472cbd3d13
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-15 15:49:25 +0200

    Updated tests

--- Labs/Lab5/tests/test_gui.py
+++ Labs/Lab5/tests/test_gui.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
-import time
 from fixtures import db_setup, driver_setup
 from helpers import assert_input, fill_out_customer_form
 
@@ -13,6 +12,8 @@ seconds_to_wait = 4
 class TestGUI:
     def test_open_exisiting_user(self, db_setup, driver_setup):
         driver = driver_setup
+
+        # get a customer to open/edit
         result = requests.get("http://localhost:6399/customers")
         assert result.ok
         assert result.status_code == 200
@@ -23,12 +24,8 @@ class TestGUI:
         customer = customers[0]
         assert customer is not None
 
-        # select customer
-        try:
-            customer_element = WebDriverWait(driver, seconds_to_wait).until(EC.presence_of_element_located((By.ID, customer["ID"])))
-        except TimeoutException:
-            print("Loading took too much time!")
-
+        # edit customer
+        customer_element = driver.find_element_by_id(customer["ID"])
         customer_element.click()
 
         data = {
@@ -42,12 +39,15 @@ class TestGUI:
             "city": customer["City"],
             "email": customer["Email"],
         }
+
+        # make sure the input fields have the correct data
         assert_input(driver, data)
 
         # click edit
         edit_element = driver.find_element_by_id("edit_customer_btn")
         edit_element.click()
 
+        # fill out customer form and save
         fill_out_customer_form(driver)
 
         driver.close()
@@ -56,12 +56,10 @@ class TestGUI:
         driver = driver_setup
 
         # click create new customer button
-        try:
-            create_element = WebDriverWait(driver, seconds_to_wait).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[1]/button")))
-        except TimeoutException:
-            print("Loading took too much time!")
+        create_element = driver.find_element_by_xpath("/html/body/div/div/div[1]/button")
         create_element.click()
 
+        # fill out customer form and save
         fill_out_customer_form(driver)
 
         driver.close()
@@ -69,19 +67,15 @@ class TestGUI:
     def test_delete_customer(self, db_setup, driver_setup):
         driver = driver_setup
 
-        # wait for page reload and get customer list
-        try:
-            customer_list_element = WebDriverWait(driver, seconds_to_wait).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[1]")))
-        except TimeoutException:
-            print("Loading took too much time!")
-
         # find all customers
+        customer_list_element = driver.find_element_by_xpath("/html/body/div/div/div[1]")
         customer_elements = customer_list_element.find_elements_by_tag_name("p")
         customer_count = len(customer_elements)
         assert customer_count > 0
 
         # select first customer
         customer_element = customer_elements[0]
+        customer_id = customer_element.get_attribute("id")
         customer_element.click()
 
         # click delete
@@ -93,17 +87,22 @@ class TestGUI:
         assert alert.text == "Are you sure you want to delete this customer?"
         alert.accept()
 
-        # wait for page to refresh
-        time.sleep(1)
-
-        # wait for page reload and get customer list
+        # wait for save request to finish and trigger reload
         try:
-            customer_list_element = WebDriverWait(driver, seconds_to_wait).until(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[1]")))
+            WebDriverWait(driver, seconds_to_wait).until(EC.staleness_of(customer_list_element))
         except TimeoutException:
-            print("Loading took too much time!")
+            print("Took too long to wait for refresh")
+
+        # wait for vue to render, the v-for div should be removed when done
+        try:
+            customer_list_is_empty = WebDriverWait(driver, seconds_to_wait).until_not(EC.presence_of_element_located((By.XPATH, "/html/body/div/div/div[1]/div[1]")))
+        except TimeoutException:
+            print("Took too long to find customer_list_element")
 
         # confirm the user was deleted
-        customer_elements = customer_list_element.find_elements_by_tag_name("p")
+        assert customer_list_is_empty == True
+        customer_elements = driver.find_element_by_xpath("/html/body/div/div/div[1]").find_elements_by_tag_name("p")
         assert len(customer_elements) < customer_count
+        assert len(driver.find_elements_by_id(customer_id)) == 0
 
         driver.close()
