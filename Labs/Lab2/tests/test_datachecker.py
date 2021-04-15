6ba2afe5bed2b393bbdcefc112f2f413beaa4bf9
Teo Carlsson <carlsson.teo@gmail.com>
2021-04-15 11:52:48 +0200

    Seminar feedback: one-assert-per-method, boundary testing

--- Labs/Lab2/tests/test_datachecker.py
+++ Labs/Lab2/tests/test_datachecker.py
@@ -7,33 +7,50 @@ class TestDataChecker:
     def setup_method(self, method, mocked_connect_to_database):
         self.dc = DataChecker()
 
-    def test_check_valid_age(self):
-        dc = self.dc
+    def test_check_valid_age_invalid_type_string(self):
+        assert self.dc.check_valid_age("1") == False
 
-        assert dc.check_valid_age(1) == True
-        assert dc.check_valid_age(-1) == False
-        assert dc.check_valid_age(1.5) == False
-        assert dc.check_valid_age("1") == False
+    def test_check_valid_age_invalid_type_float(self):
+        assert self.dc.check_valid_age(1.5) == False
 
-    def test_check_valid_text_field(self):
-        dc = self.dc
+    def test_check_valid_age_negative_integer_small(self):
+        assert self.dc.check_valid_age(-1) == False
 
-        assert dc.check_valid_text_field("") == False
-        assert dc.check_valid_text_field("not empty") == True
-        assert dc.check_valid_text_field("", False) == True
-        assert dc.check_valid_text_field("not empty", False) == True
+    def test_check_valid_age_negative_integer_large(self):
+        assert self.dc.check_valid_age(-1000) == False
+
+    def test_check_valid_age_positive_integer_zero(self):
+        assert self.dc.check_valid_age(0) == True
+
+    def test_check_valid_age_positive_integer_small(self):
+        assert self.dc.check_valid_age(1) == True
+
+    def test_check_valid_age_positive_integer_large(self):
+        assert self.dc.check_valid_age(1000) == True
+
+    def test_check_valid_text_field_empty_default(self):
+        assert self.dc.check_valid_text_field("") == False
+
+    def test_check_valid_text_field_empty_must_not_have_content(self):
+        assert self.dc.check_valid_text_field("", False) == True
+
+    def test_check_valid_text_field_content_default(self):
+        assert self.dc.check_valid_text_field("not empty") == True
+
+    def test_check_valid_text_field_content_must_not_have_content(self):
+        assert self.dc.check_valid_text_field("not empty", False) == True
 
     @patch.object(DataChecker, "get_customer")
-    @patch.object(DataChecker, "get_equipment")
-    def test_customer_has_equipment_attached(self, mocked_get_customer, mocked_get_equipment):
-        dc = self.dc
-
+    def test_customer_has_equipment_attached_invalid_customer(self, mocked_get_customer):
         # invalid customer
-        dc.get_customer.return_value = []
-        assert dc.customer_has_equipment_attached(0) == False
+        self.dc.get_customer.return_value = []
 
-        # valid customer - invalid equipment pointer
-        dc.get_customer.return_value = [
+        assert self.dc.customer_has_equipment_attached(0) == False
+
+    @patch.object(DataChecker, "get_customer")
+    def test_customer_has_equipment_attached_valid_customer_invalid_ptr(self, mocked_get_customer):
+        # valid customer - no equipment pointer
+        self.dc.get_customer.return_value = [
             # customer
             [
                 0, "", "", 0, "", "", "", "", "", None,
@@ -41,16 +58,44 @@ class TestDataChecker:
                 None, "", ""
             ]
         ]
-        assert dc.customer_has_equipment_attached(0) == False
 
-        # valid customer - valid equipment pointer - invalid equipment
-        dc.get_customer.return_value[0][10] = 0 # set valid equipment pointer
-        dc.get_equipment.return_value = [] # no equipment
-        assert dc.customer_has_equipment_attached(0) == False
+        assert self.dc.customer_has_equipment_attached(0) == False
 
-        # valid customer - valid equipment pointer - valid equipment
-        dc.get_equipment.return_value = [
-            # set some valid equipment
+    @patch.object(DataChecker, "get_customer")
+    @patch.object(DataChecker, "get_equipment")
+    def test_customer_has_equipment_attached_valid_customer_valid_ptr_invalid_equipment(self, mocked_get_customer, mocked_get_equipment):
+        # valid customer - valid equipment pointer
+        self.dc.get_customer.return_value = [
+            # customer
+            [
+                0, "", "", 0, "", "", "", "", "", None,
+                0, # valid equipment pointer
+                None, "", ""
+            ]
+        ]
+
+        # invalid equipment
+        self.dc.get_equipment.return_value = []
+
+        assert self.dc.customer_has_equipment_attached(0) == False
+
+    @patch.object(DataChecker, "get_customer")
+    @patch.object(DataChecker, "get_equipment")
+    def test_customer_has_equipment_attached_valid_all(self, mocked_get_customer, mocked_get_equipment):
+        # valid customer - valid equipment pointer
+        self.dc.get_customer.return_value = [
+            # customer
+            [
+                0, "", "", 0, "", "", "", "", "", None,
+                0, # valid equipment pointer
+                None, "", ""
+            ]
+        ]
+
+        # valid equipment
+        self.dc.get_equipment.return_value = [
+            # some equipment
             []
         ]
-        assert dc.customer_has_equipment_attached(0) == True
+
+        assert self.dc.customer_has_equipment_attached(0) == True
